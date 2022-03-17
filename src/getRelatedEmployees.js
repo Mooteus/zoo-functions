e676c350477ed1d59f2744c334f9cb1efbdd7eae
Mooteus <moteus10@gmail.com>
2022-03-17 16:46:55 -0300

    feat: add isManager function in getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const managersArray = [];
+  employees.forEach((element) => {
+    managersArray.push(...element.managers);
+  });
+  if (managersArray.find((people) => people === id)) return true;
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
+
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = { isManager, getRelatedEmployees };
