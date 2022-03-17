cae92221d8ed4982ca17a5040e0cd28c9fa6cb05
Mooteus <moteus10@gmail.com>
2022-03-17 18:00:43 -0300

    feat: add searchRelatedEmployees and getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,17 +2,33 @@ const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
   const managersArray = [];
+
   employees.forEach((element) => {
     managersArray.push(...element.managers);
   });
+
   if (managersArray.find((people) => people === id)) return true;
   return false;
 }
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+function searchRelatedEmployees(managerId) {
+  const peopleName = [];
+  employees.forEach((element) => {
+    if (element.managers[0] === managerId || element.managers[1] === managerId) {
+      peopleName.push(`${element.firstName} ${element.lastName}`);
+    }
+  });
+  return peopleName;
 }
 
-console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+function getRelatedEmployees(managerId) {
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    return searchRelatedEmployees(managerId);
+  }
+}
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
