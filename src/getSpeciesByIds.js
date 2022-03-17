693d3aebb0aadda4e0acdc9dd06a6723b2854e62
Mooteus <moteus10@gmail.com>
2022-03-17 14:20:45 -0300

    refactor: getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,21 +4,12 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   const animals = [];
 
-  if (!ids) {
-    return animals;
-  }
-
-  if (typeof ids === 'string') {
-    animals.push(species.find((animal) => animal.id === ids));
-    return animals;
-  }
-
   ids.forEach((element) => {
     animals.push(species.find((animal) => animal.id === element));
   });
   return animals;
 }
 
-// console.log(getSpeciesByIds(['0938aa23-f153-4937-9f88-4858b24d6bce']));
+console.log(getSpeciesByIds(['0938aa23-f153-4937-9f88-4858b24d6bce']));
 
 module.exports = getSpeciesByIds;
