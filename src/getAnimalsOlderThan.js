971b54874eac32cbdc3c5e003afff9a99351fc78
Mooteus <moteus10@gmail.com>
2022-03-17 14:36:57 -0300

    feat: add funtion getAniamlsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalSelected = species.find((name) => name.name === animal);
+  const animalResidents = animalSelected.residents;
+
+  return animalResidents.every((animalAge) => animalAge.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
