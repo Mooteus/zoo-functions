4fee6ec7809c138bb3f0b77fe46a717c0e548432
Mooteus <moteus10@gmail.com>
2022-03-18 14:47:10 -0300

    feat: add getAnimalPerLocation

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,28 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getAnimalPerLocation(location) {
+  const animalsName = [];
+  const animals = species.filter((animal) => animal.location === location);
+  animals.forEach((element) => {
+    animalsName.push(element.name);
+  });
+  return animalsName;
 }
 
+function getAnimalMap(options) {
+  const objResult = {
+    NE: getAnimalPerLocation('NE'),
+    NW: getAnimalPerLocation('NW'),
+    SE: getAnimalPerLocation('SE'),
+    SW: getAnimalPerLocation('SW'),
+  };
+  if (!options) {
+    return objResult;
+  }
+  const hasIncludeNames = Object.prototype.hasOwnProperty.call(options, 'includeNames');
+  if (!hasIncludeNames) {
+    return objResult;
+  }
+}
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
