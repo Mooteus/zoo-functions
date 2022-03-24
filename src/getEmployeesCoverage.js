4ae5e96137bc15127aab2be063d1a337ecb1ca29
Mooteus <moteus10@gmail.com>
2022-03-24 00:36:11 -0300

    feat: get people infos by name

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,31 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getPeopleInfo(obj) {
+  let employee = employees.find((emp) => emp.firstName === obj.name);
+  if (!employee) employee = employees.find((emp) => emp.lastName === obj.name);
+
+  const locationArray = [];
+  const animalNameArray = [];
+
+  employee.responsibleFor.forEach((element) => {
+    locationArray.push(species.find((animal) => animal.id === element).location);
+    animalNameArray.push(species.find((animal) => animal.id === element).name);
+  });
+
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: animalNameArray,
+    locations: locationArray };
 }
+
+function getEmployeesCoverage(obj) {
+  if (Object.prototype.hasOwnProperty.call(obj, 'name')) {
+    return getPeopleInfo(obj);
+  }
+}
+
+console.log(getEmployeesCoverage({ name: 'Strauss' }));
 
 module.exports = getEmployeesCoverage;
