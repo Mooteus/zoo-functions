690308a61795402de9a146e67aead6ff2f758e9c
Mooteus <moteus10@gmail.com>
2022-03-23 22:22:57 -0300

    feat: get oldest animal from first specie

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,24 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((emp) => emp.id === id);
+  const animalId = employee.responsibleFor[0];
+  const animals = species.find((animal) => animal.id === animalId);
+
+  let animalAge = animals.residents[0].age;
+  let animalOld = animals.residents[0].name;
+  let animalSex = animals.residents[0].sex;
+
+  animals.residents.forEach((element) => {
+    if (animalAge < element.age) {
+      animalAge = element.age;
+      animalOld = element.name;
+      animalSex = element.sex;
+    }
+  });
+
+  return [animalOld, animalSex, animalAge];
 }
 
 module.exports = getOldestFromFirstSpecies;
